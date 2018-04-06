dac184c59f4061ca019fa96cfe82ad0bab7e74bc
Bunlong <bunlong.van@gmail.com>
2018-04-06 11:50:36 +0700

    init

--- src/App.js
+++ src/App.js
@@ -1,6 +1,4 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
 import { Line, Bar, Pie, Radar, PolarArea, Doughnut, HorizontalBar } from 'react-chartjsx';
 
 class App extends Component {
