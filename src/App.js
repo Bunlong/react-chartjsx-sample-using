1d168ffc40d93b01125e9518ea1c7682570963dc
Bunlong <bunlong.van@gmail.com>
2018-04-09 00:32:24 +0700

    add bubble and scatter

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-import { Line, Bar, Pie, Radar, PolarArea, Doughnut, HorizontalBar, Bubble } from 'react-chartjsx';
+import { Line, Bar, Pie, Radar, PolarArea, Doughnut, HorizontalBar, Bubble, Scatter } from 'react-chartjsx';
 
 class App extends Component {
   constructor(props) {
@@ -19,27 +19,32 @@ class App extends Component {
     },
     lineChartData: {
       labels: [1500, 1600, 1700, 1750, 1800, 1850, 1900, 1950, 1999, 2050],
-      datasets: [{ 
+      datasets: [
+        { 
           data: [86, 114, 106, 106, 107, 111, 133, 221, 783, 2478],
           label: "Africa",
           borderColor: "#3e95cd",
           fill: false
-        }, { 
+        }, 
+        { 
           data: [282, 350, 411, 502, 635, 809, 947, 1402, 3700, 5267],
           label: "Asia",
           borderColor: "#8e5ea2",
           fill: false
-        }, { 
+        }, 
+        { 
           data: [168, 170, 178, 190, 203, 276, 408, 547, 675, 734],
           label: "Europe",
           borderColor: "#3cba9f",
           fill: false
-        }, { 
+        }, 
+        { 
           data: [40, 20, 10, 16, 24, 38, 74, 167, 508, 784],
           label: "Latin America",
           borderColor: "#e8c3b9",
           fill: false
-        }, { 
+        }, 
+        { 
           data: [6, 3, 2, 2, 7, 26, 82, 172, 312, 433],
           label: "North America",
           borderColor: "#c45850",
@@ -181,7 +186,8 @@ class App extends Component {
             y: 6.994,
             r: 15
           }]
-        }, 
+        }
+        , 
         {
           label: ["Japan"],
           backgroundColor: "rgba(193,46,12,0.2)",
@@ -191,6 +197,55 @@ class App extends Component {
             y: 5.921,
             r: 15
           }]
+        }
+      ]
+    },
+    scatterChartData: {
+      datasets: [
+        {
+          label: 'Scatter Dataset',
+          borderColor: "#3e95cd",
+          data: [
+            {
+              x: -10,
+              y: 0
+            }, 
+            {
+              x: 0,
+              y: 10
+            }, 
+            {
+              x: 10,
+              y: 5
+            },
+            {
+              x: 20,
+              y: 6
+            }
+          ]
+        },
+        {
+          label: 'Scatter1 Dataset',
+          borderColor: "#8e5ea2",
+          data: [
+            {
+              x: -11,
+              y: 1
+            }, 
+            {
+              x: 1,
+              y: 11
+            }, 
+            {
+              x: 11,
+              y: 6
+            }
+            ,
+            {
+              x: 21,
+              y: 7
+            }
+          ]
         }
       ]
     }
@@ -386,48 +441,143 @@ class App extends Component {
 
   clickBubble() {
     this.setState({
-      datasets: [
-        {
-          label: ["China"],
-          backgroundColor: "rgba(255,221,50,0.2)",
-          borderColor: "rgba(255,221,50,1)",
-          data: [{
-            x: 21269017,
-            y: 5.245,
-            r: 15
-          }]
-        }, 
-        {
-          label: ["Denmark"],
-          backgroundColor: "rgba(60,186,159,0.2)",
-          borderColor: "rgba(60,186,159,1)",
-          data: [{
-            x: 258702,
-            y: 7.526,
-            r: 10
-          }]
-        }, 
-        {
-          label: ["Germany"],
-          backgroundColor: "rgba(0,0,0,0.2)",
-          borderColor: "#000",
-          data: [{
-            x: 3979083,
-            y: 6.994,
-            r: 15
-          }]
-        }, 
-        {
-          label: ["Japan"],
-          backgroundColor: "rgba(193,46,12,0.2)",
-          borderColor: "rgba(193,46,12,1)",
-          data: [{
-            x: 4931877,
-            y: 5.921,
-            r: 35
-          }]
-        }
-      ]
+      bubbleChartData: {
+        datasets: [
+          {
+            label: ["China"],
+            backgroundColor: "rgba(255,221,50,0.2)",
+            borderColor: "rgba(255,221,50,1)",
+            data: [{
+              x: 21269017,
+              y: 5.245,
+              r: 15
+            }]
+          }, 
+          {
+            label: ["Denmark"],
+            backgroundColor: "rgba(60,186,159,0.2)",
+            borderColor: "rgba(60,186,159,1)",
+            data: [{
+              x: 258702,
+              y: 7.526,
+              r: 10
+            }]
+          }, 
+          {
+            label: ["Germany"],
+            backgroundColor: "rgba(0,0,0,0.2)",
+            borderColor: "#000",
+            data: [{
+              x: 3979083,
+              y: 6.994,
+              r: 15
+            }]
+          }, 
+          {
+            label: ["Japan"],
+            backgroundColor: "rgba(193,46,12,0.2)",
+            borderColor: "rgba(193,46,12,1)",
+            data: [{
+              x: 4931877,
+              y: 5.921,
+              r: 15
+            }]
+          },
+          {
+            label: ["Cambodia"],
+            backgroundColor: "rgba(193,46,12,0.2)",
+            borderColor: "rgba(193,46,12,1)",
+            data: [{
+              x: 1931877,
+              y: 5.921,
+              r: 15
+            }]
+          },
+          {
+            label: ["VN"],
+            backgroundColor: "rgba(193,46,12,0.2)",
+            borderColor: "rgba(193,46,12,1)",
+            data: [{
+              x: 1931877,
+              y: 2.921,
+              r: 15
+            }]
+          }
+        ]
+      }
+    })
+  }
+
+  clickScatter() {
+    this.setState({
+      scatterChartData: {
+        datasets: [
+          {
+            label: 'Scatter Dataset',
+            borderColor: "#3e95cd",
+            data: [
+              {
+                x: -10,
+                y: 0
+              }, 
+              {
+                x: 0,
+                y: 10
+              }, 
+              {
+                x: 10,
+                y: 5
+              }
+              ,
+              {
+                x: 20,
+                y: 6
+              }
+            ]
+          },
+          {
+            label: 'Scatter1 Dataset',
+            borderColor: "#8e5ea2",
+            data: [
+              {
+                x: -11,
+                y: 1
+              }, 
+              {
+                x: 1,
+                y: 11
+              }, 
+              {
+                x: 11,
+                y: 6
+              }
+              ,
+              {
+                x: 21,
+                y: 7
+              }
+            ]
+          },
+          {
+            label: 'Scatter1 Dataset',
+            borderColor: "#8e5ea2",
+            data: [
+              {
+                x: -12,
+                y: 2
+              }, 
+              {
+                x: 2,
+                y: 12
+              }, 
+              {
+                x: 12,
+                y: 7
+              }
+            ]
+          }
+        ]
+      }
     })
   }
 
@@ -488,6 +638,11 @@ class App extends Component {
                 width={800} 
                 height={400} />
         <button onClick={this.clickBubble.bind(this)}>Click bubble</button>
+        <Scatter data={this.state.scatterChartData} 
+                 options={chartOptions} 
+                 width={800} 
+                 height={400} />
+        <button onClick={this.clickScatter.bind(this)}>Click scatter</button>
       </div>
     );
   }
